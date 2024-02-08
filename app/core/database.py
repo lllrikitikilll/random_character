702f52dededc8e10ca2d1fce761df6ca23b13a3d
lllrikitikilll <lllrikitikilll@yandex.ru>
2024-02-08 16:38:25 +0300

    Create model user

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,15 @@
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy import create_engine
 from os import getenv
 from dotenv import load_dotenv
 
 load_dotenv('.env')
+
+
 ENGINE_URL = getenv('POSTGRESQL_ENGINE')
+
+class Base(DeclarativeBase):
+    pass
 
 
 engine = create_engine(ENGINE_URL)
---
+++ app/users/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(60))
+
